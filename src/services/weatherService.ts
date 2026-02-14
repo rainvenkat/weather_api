eb86f78f553d100d9334547a8030168ac7f3a1b5
rainvenkat <ranivenkat1@gmail.com>
2026-02-14 17:04:16 +0000

    Update weatherService.ts

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -25,3 +25,27 @@ export const generateDublinWeatherData = (): WeatherData => {
   // Return weather data
   return generatedWeatherData;
 };
+export const generateGermanyWeatherData = (): WeatherData => {
+  // Generate random weather data
+  const generatedWeatherData = {
+    temperature: faker.number.int({ min: -15, max: 30 }),
+    humidity: faker.number.int({ min: 79, max: 86 }),
+    wind: faker.number.int({ min: 2, max: 78 }),
+    rain: faker.number.int({ min: 65, max: 75 }),
+  };
+
+  // Return weather data
+  return generatedWeatherData;
+};
+export const generateSpainWeatherData = (): WeatherData => {
+  // Generate random weather data
+  const generatedWeatherData = {
+    temperature: faker.number.int({ min: -15, max: 30 }),
+    humidity: faker.number.int({ min: 79, max: 86 }),
+    wind: faker.number.int({ min: 2, max: 78 }),
+    rain: faker.number.int({ min: 65, max: 75 }),
+  };
+
+  // Return weather data
+  return generatedWeatherData;
+};
